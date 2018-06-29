63af77fdfb9f28f14de0a523b13dc8cfc424ea74
Eddie Antonio Santos <easantos@ualberta.ca>
2018-06-29 13:22:12 -0600

    Write ᐚᐸᒣᐤ regression test.

---
+++ neahtta/cypress/integration/regression-tests.js
@@ -0,0 +1,46 @@
+/**
+ * Tests for previous app regressions that should continue working.
+ */
+
+describe('Display of syllabic codas', function () {
+  it('should render wâpamêw+V+TA+Ind+Prs+3Sg+4Sg/PlO as ᐘᐸᒣᐤ', function () {
+    cy.visit('http://localhost:5000/itwewina');
+
+    cy.get('#left_nav')
+      .contains('ᒐᐦᑲᓯᓇᐦᐃᑲᐣ') // Use syllabics written variant.
+      .click();
+
+    // Type "ᐘᐸᒣᐤ" in the search box, and do the search.
+    cy.get('form#neahttasaanit')
+      .get('input[name=lookup]')
+      .type('ᐘᐸᒣᐤ');
+    cy.get('form#neahttasaanit')
+      .contains('.hidden-phone *', 'Search') // Click the desktop visible link
+      .click();
+
+    // Should have gone to the search results page.
+    // XXX: Navigating doesn't change the URL, so the best thing to do is to
+    // check if a result appeared on the page.
+    cy.contains(/√wâpam/);
+
+    // Click the search result for "wâpamêw".
+    cy.contains('ᐚᐸᒣᐤ')
+      .click();
+
+    var ᐘᐸᒣᐤ_percent_encoded = encodeURIComponent('ᐚᐸᒣᐤ');
+
+    // We should navigate to a page.
+    cy.url()
+      .should('include', 'detail')
+      .should('include', ᐘᐸᒣᐤ_percent_encoded);
+
+    // Get the full paradigm.
+    cy.contains('full')
+      .click();
+
+    // This row should have the proper derivation!
+    cy.contains('3s → 4')
+      .parents('tr').first()
+      .contains('ᐚᐸᒣᐤ');
+  });
+});
