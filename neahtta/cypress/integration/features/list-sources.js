9d52a428481ff662df32f6653ed9a0ac8d501f6c
Eddie Antonio Santos <easantos@ualberta.ca>
2018-07-05 13:12:59 -0600

    Write failing test for source on details page.

--- neahtta/cypress/integration/features/list-sources.js
+++ neahtta/cypress/integration/features/list-sources.js
@@ -4,7 +4,7 @@
  */
 
 describe("List of an entry's sources", function () {
-  it.only('should not be displayed on the search results page', function () {
+  it('should not be displayed on the search results page', function () {
     cy.visit('/crk/eng');
     cy.neahttaSearch('wanâpamow');
 
@@ -15,5 +15,17 @@ describe("List of an entry's sources", function () {
     // We should not know that from the front page.
     cy.get('.lexeme')
       .should('not.contain', 'nêhiyawêwin : itwêwina / Cree : Word');
+  });
+
+  it('should be displayed on the details page', function () {
+    var detailURL = '/detail/crk/eng/wan%C3%A2pamow.html?no_compounds=true&lemma_match=true&e_node=5556836001097474918';
+    cy.visit(detailURL);
+
+    // We should be on the details page.
+    cy.contains('wanâpamow');
+
+    // We should see the source on the page.
+    cy.get('.lexeme')
+      .should('contain', 'nêhiyawêwin : itwêwina / Cree : Word');
   });
 });
