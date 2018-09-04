e5de15abe6bbf559fcca2566acd61c23ec691d59
Eddie Antonio Santos <easantos@ualberta.ca>
2018-09-04 10:46:33 -0600

    Revert "Create and instantiate AlternateOrthographyDict; implementation incomplete."
    This reverts commit f2824f1c92f1d7b9f7ed011afbe047f424b75367.

--- neahtta/config.py
+++ neahtta/config.py
@@ -573,10 +573,11 @@ class Config(Config):
                                 'orig_pair': (source, target),
                                 'path': path,
                             }
-                            # Keep track of what orthography this variant dictionary uses.
-                            derivative = v.get('derivative_orthography', None)
-                            if derivative is not None:
-                                info['derivative_orthography'] = derivative
+                            # Keep track of the dictionary that this is a variant of.
+                            # This presupposes that the "canonical dictionary" list is already parsed.
+                            derivative_of = v.get('derive_from', None)
+                            if derivative_of is not None:
+                                info['dictionary'] =self.dictionaries[(source, target)]
                             language_pairs[(v.get('short_name'), target)] = info
                     else:
                         # This is the "source" dictinary..
