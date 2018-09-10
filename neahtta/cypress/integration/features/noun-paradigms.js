c62525b0acce0a3aad12823c5e79469753377233
Eddie Antonio Santos <easantos@ualberta.ca>
2018-09-10 15:34:45 -0600

    Try writing neat test, but it doesn't work...

---
+++ neahtta/cypress/integration/features/noun-paradigms.js
@@ -0,0 +1,28 @@
+describe("The basic noun paradigms", function () {
+
+  it('should display all NI forms', function () {
+    cy.instantNeahttaSearch('crk', 'eng', 'astotin');
+    cy.contains('a', 'astotin')
+      .click();
+
+    findRowInBasicParadigm('Singular', 'astotin');
+    findRowInBasicParadigm('Plural', 'astotina');
+    findRowInBasicParadigm('Locative', 'astotinihk');
+    findRowInBasicParadigm('1s	poss (sg)', 'nitastotin');
+    findRowInBasicParadigm('2s	poss (sg)', 'kitastotin');
+    findRowInBasicParadigm('3s	poss (sg)', 'otastotin');
+  });
+
+  function findRowInBasicParadigm(header, wordform) {
+    // ensure this header is on the page.
+    cy.get('table[data-type="basic"]')
+      .contains('th a, th', header)
+      .should('be.visible')
+      .then($cell => {
+        let $headerCell = $cell.is('th') ? $cell : $cell.closest('th');
+        if (!$cell.is('th')) debugger;
+        let $wordforms = $headerCell.siblings('td');
+        expect($wordforms.text()).to.include(wordform);
+      });
+  };
+});
