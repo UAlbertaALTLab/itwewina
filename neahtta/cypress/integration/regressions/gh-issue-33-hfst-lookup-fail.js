7d13dae592be111e7b0a1486cd38360b36e3fc7e
Eddie Antonio Santos <easantos@ualberta.ca>
2018-09-10 13:20:24 -0600

    Test whether tags are output on a VTA paradigm table.

---
+++ neahtta/cypress/integration/regressions/gh-issue-33-hfst-lookup-fail.js
@@ -0,0 +1,16 @@
+describe('When hfst does not match a form', function () {
+  it('should display as hyphens in the paradigm', function () {
+    // TODO: This might need a better example of a word with out an analysis.
+    cy.visit('/crk/eng?lookup=' + encodeURIComponent('wâpamêw'));
+    cy.contains('a', 'wâpamêw')
+      .click();
+
+    // View the full paradigm
+    cy.get('.nav')
+      .contains('full').click();
+
+    // There might be this mess of tags here; it really should not be there.
+    cy.get('#paradigm-tab-3 > .miniparadigm')
+      .should('not.contain', 'Imp+Imm+2Sg+4Sg/PlO');
+  });
+});
