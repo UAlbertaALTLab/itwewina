0ed986e108abbc0e0bebd4b7ed87f3c01edc7548
Eddie Antonio Santos <easantos@ualberta.ca>
2018-09-12 09:08:37 -0600

    Add MESC logo to footer.

--- neahtta/cypress/integration/features/gh-issue-50-mesc.js
+++ neahtta/cypress/integration/features/gh-issue-50-mesc.js
@@ -5,4 +5,20 @@ describe('Our collaboration with MESC', function () {
     cy.contains('Maskwacîs Education Schools Commission')
       .should('be.visible');
   });
+
+  it('should be displayed as a logo on the about page', function () {
+    cy.visit('/about/');
+
+    // It should have an alt caption with "Maskwacîs" in it. 
+    cy.get('img[alt~="Maskwacîs" i]')
+      .should('be.visible');
+  });
+
+  it('should be displayed as a logo on the front page', function () {
+    cy.visit('/');
+
+    // It should have an alt caption with "Maskwacîs" in it. 
+    cy.get('img[alt~="Maskwacîs" i]')
+      .should('be.visible');
+  });
 });
