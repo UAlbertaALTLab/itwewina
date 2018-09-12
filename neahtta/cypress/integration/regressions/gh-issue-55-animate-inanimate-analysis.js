766915f3174caf29012173fc95b0832db16cd3c1
Eddie Antonio Santos <easantos@ualberta.ca>
2018-09-12 14:53:49 -0600

    Add test for nouns that are both animate and inanimate.

---
+++ neahtta/cypress/integration/regressions/gh-issue-55-animate-inanimate-analysis.js
@@ -0,0 +1,48 @@
+/**
+ * Tests that dictionary entries with ambiguous animacy have the correct
+ * analysis.
+ *
+ * See: https://github.com/UAlbertaALTLab/itwewina/issues/55
+ */
+
+describe('Nouns of ambiguous animacy', function () {
+  /*
+   * "mitâs" is both an animate and an inanimate noun.
+   *
+   * mitâs (NDA-1) -- legging/gaiter
+   * mitâs (NDI-1) -- pair of pants
+   */
+  it('should provide the "inanimate" tag for the inanimate form', function () {
+    cy.instantNeahttaSearch('crk', 'eng', 'mitâs');
+    cy.contains('a', /mitâs.+NDI/)
+      .click();
+
+    cy.url()
+      .should('contain', 'detail');
+    cy.contains(/mitâs.+NDI-1/);
+    // We're on the detail page now for the INANIMATE noun.
+    
+    cy.get('.possible_analyses').as('analyses');
+
+    cy.get('@analyses')
+      .contains('Inanimate');
+    // TODO: check that there is only ONE analysis
+  });
+
+  it('should provide the "animate" tag for the animate form', function () {
+    cy.instantNeahttaSearch('crk', 'eng', 'mitâs');
+    cy.contains('a', /mitâs.+NDA/)
+      .click();
+
+    cy.url()
+      .should('contain', 'detail');
+    cy.contains(/mitâs.+NDA-1/);
+    // We're on the detail page now for the ANIMATE noun.
+    
+    cy.get('.possible_analyses').as('analyses');
+
+    cy.get('@analyses')
+      .contains('Animate');
+    // TODO: check that there is only ONE analysis
+  });
+});
