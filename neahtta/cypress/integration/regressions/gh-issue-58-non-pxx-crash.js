d2f0a28d56a27a9612f2d6a9604c0c3c02e0f80b
Eddie Antonio Santos <easantos@ualberta.ca>
2018-09-13 11:20:16 -0600

    Fix comments in regression test.

--- neahtta/cypress/integration/regressions/gh-issue-58-non-pxx-crash.js
+++ neahtta/cypress/integration/regressions/gh-issue-58-non-pxx-crash.js
@@ -1,10 +1,14 @@
 /**
- * Non-PxX words crash itêwina search.
+ * Non-PxX words crash itwêwina search.
+ *
+ * See: https://github.com/UAlbertaALTLab/itwewina/issues/58
  */
 describe('Searching for non-PxX words', function () {
   it('should display search results for words like "-okom"', function () {
     cy.visit('/');
     cy.neahttaSearch('nohkom');
+    // Previously, the search would crash here, before getting to the results
+    // page.
 
     cy.contains('a', 'nôhkom');
   });
