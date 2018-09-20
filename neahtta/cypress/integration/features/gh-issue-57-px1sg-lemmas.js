a2a7d00801446dd4861015332775d3b5eeb505b4
Eddie Antonio Santos <easantos@ualberta.ca>
2018-09-20 12:04:25 -0600

    Test for non-PxX NDAs with Px1Sg lemmas. See #57.

---
+++ neahtta/cypress/integration/features/gh-issue-57-px1sg-lemmas.js
@@ -0,0 +1,25 @@
+/**
+ * Nouns with OBLIGATORY POSSESSION and cannot have "unspecified
+ * possessor" ("non-PxX nouns") should link to their lemma, which is in the
+ * first-person possessor form ("Px1Sg").
+ *
+ * For example, "kôhkom" is *your* grandmother. There can't just be a 
+ * "grandmother"; she has to be my grandmother, your grandmother, or their
+ * grandmother! These are non-PxX nouns.
+ *
+ * Historically, we'd present non-PxX nouns as the stem, and link to the
+ * Px1Sg form of the noun with a <lemma_ref> element in the dictionary, but
+ * this turns out to be inelegant.
+ * 
+ * See: https://github.com/UAlbertaALTLab/itwewina/issues/57
+ */
+describe('Non-PxX NDA nouns', function () {
+  it('should have lemmas in the Px1Sg form', function () {
+    // Search for "kokom", which should match the Px2Sg form of "nôhkom".
+    cy.instantNeahttaSearch('crk', 'eng', 'kokom');
+
+    // The lexical entry should be nôhkom! The Px1Sg form of the word.
+    cy.get('.lexeme')
+      .contains('a', /nôhkom\s+[(]Noun/);
+  });
+});
