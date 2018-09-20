7aa7a6ff5d569ebcf0204b162ecec4f5309ba105
Eddie Antonio Santos <easantos@ualberta.ca>
2018-09-20 15:23:16 -0600

    Update test for #7 using latest test writing conventions.

--- neahtta/cypress/integration/features/gh-issue-7-list-entry-sources.js
+++ neahtta/cypress/integration/features/gh-issue-7-list-entry-sources.js
@@ -1,12 +1,13 @@
 /**
  * Tests the display (or omission of display) of the sources for a particular
- * entry's text/
+ * entry's text.
+ *
+ * See: https://github.com/UAlbertaALTLab/itwewina/issues/7
  */
 
 describe("List of an entry's sources", function () {
   it('should not be displayed on the search results page', function () {
-    cy.visit('/crk/eng');
-    cy.neahttaSearch('wanâpamow');
+    cy.instantNeahttaSearch('crk', 'eng', 'wanâpamow');
 
     // We should be on the results page.
     cy.contains('√wanâpamo-');
@@ -18,11 +19,13 @@ describe("List of an entry's sources", function () {
   });
 
   it('should be displayed on the details page', function () {
-    var detailURL = '/detail/crk/eng/wan%C3%A2pamow.html?no_compounds=true&lemma_match=true&e_node=5556836001097474918';
-    cy.visit(detailURL);
+    cy.instantNeahttaSearch('crk', 'eng', 'wanâpamow');
+    cy.contains('a', 'wanâpamow')
+      .click();
 
     // We should be on the details page.
-    cy.contains('wanâpamow');
+    cy.url()
+      .should('contain', 'detail');
 
     // We should see the source on the page.
     cy.get('.lexeme')
