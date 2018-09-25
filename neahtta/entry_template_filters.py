759351a20227767619b4a6c36aa439452d064ae0
Eddie Antonio Santos <easantos@ualberta.ca>
2018-09-25 12:58:10 -0600

    Add GPLv3 license stub to all Python files. Closes #69.

--- neahtta/entry_template_filters.py
+++ neahtta/entry_template_filters.py
@@ -1,4 +1,23 @@
-﻿from flask import current_app, g
+﻿# -*- coding: UTF-8 -*-
+
+# Neahttadigisánit — online, inflectional dictionary
+# Copyright (C) 2013–2017 University of Tromsø
+# Copyright (C) 2018 University of Alberta
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from flask import current_app, g
 
 def register_template_filters(app):
 
--- neahtta/tests/hdn_lexicon.py
+++ neahtta/tests/hdn_lexicon.py
@@ -1,4 +1,20 @@
-# -*- encoding: utf-8 -*-
+# -*- coding: UTF-8 -*-
+
+# Neahttadigisánit — online, inflectional dictionary
+# Copyright (C) 2013–2017 University of Tromsø
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 # gatáa.ang ñasa'áa
 
@@ -42,7 +58,7 @@ definition_exists_tests = [
 
 # paradigm_generation_tests = [
 #     # source, target, lemma, error_msg, paradigm_test
-# 
+#
 # ###  - V: 
 #     ('kpv', 'fin', u'мунны',
 #             "kpv verbs not generating",
