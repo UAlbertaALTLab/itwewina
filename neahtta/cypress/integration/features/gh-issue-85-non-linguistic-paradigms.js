791a9b1cd7d428ac7de2cf355d35b3955f761dd1
Eddie Antonio Santos <easantos@ualberta.ca>
2018-10-12 10:39:24 -0600

    'Plain' paradigms kinda work for some of these. Addresses #85.

--- neahtta/cypress/integration/features/gh-issue-85-non-linguistic-paradigms.js
+++ neahtta/cypress/integration/features/gh-issue-85-non-linguistic-paradigms.js
@@ -2,7 +2,7 @@
  * See: https://github.com/UAlbertaALTLab/itwewina/issues/85
  */
 describe('Non-linguistic paradigms', function () {
-  it.only('should exist for na nouns', function () {
+  it('should exist for NA nouns', function () {
     cy.instantNeahttaSearch('crk', 'eng', 'miskinahk');
     cy.contains('a', 'miskinâhk').click();
 
@@ -25,9 +25,83 @@ describe('Non-linguistic paradigms', function () {
       .should('be.visible');
   });
 
-  it('should exist for nad nouns', function () {
-    // FAIL
-    cy.get('body').should('be.falsy');
+  // BLOCKED: Layout needs improvement
+  it.skip('should exist for NI nouns', function () {
+    cy.instantNeahttaSearch('crk', 'eng', 'osi');
+    cy.contains('a', 'ôsi').click();
+
+    // Will load a new page
+    cy.contains('a', 'plain')
+      .click();
+    // Try out a few of the simplified header names.
+    cy.get('.miniparadigm[data-type=plain]').as('paradigm')
+    cy.get('@paradigm')
+      .contains('th', 'One')
+      .should('be.visible');
+    cy.get('@paradigm')
+      .contains('th', 'Many')
+      .should('be.visible');
+    cy.get('@paradigm')
+      .contains('th', 'my')
+      .should('be.visible');
+    cy.get('@paradigm')
+      .contains('th', 'your')
+      .should('be.visible');
   });
 
+  // BLOCKED: Layout does not exist!
+  it.skip('should exist for NAD nouns', function () {
+    cy.instantNeahttaSearch('crk', 'eng', 'nimosom');
+    cy.contains('a', 'nimosôm').click();
+
+    // Will load a new page
+    cy.contains('a', 'plain')
+      .click();
+    // Try out a few of the simplified header names.
+    cy.get('.miniparadigm[data-type=plain]').as('paradigm')
+    cy.get('@paradigm')
+      .contains('th', 'my')
+      .should('be.visible');
+    cy.get('@paradigm')
+      .contains('th', 'your')
+      .should('be.visible');
+  });
+
+  // BLOCKED: Layout does not exist!
+  it.skip('should exist for NID nouns', function () {
+    cy.instantNeahttaSearch('crk', 'eng', 'miteh');
+    cy.contains('a', 'mitêh').click();
+
+    // Will load a new page
+    cy.contains('a', 'plain')
+      .click();
+    // Try out a few of the simplified header names.
+    cy.get('.miniparadigm[data-type=plain]').as('paradigm')
+    cy.get('@paradigm')
+      .contains('th', 'my')
+      .should('be.visible');
+    cy.get('@paradigm')
+      .contains('th', 'your')
+      .should('be.visible');
+  });
+
+  it('should exist for VTA verbs', function () {
+    cy.instantNeahttaSearch('crk', 'eng', 'miyew');
+    cy.contains('a', 'miyêw').click();
+
+    // Will load a new page
+    cy.contains('a', 'plain')
+      .click();
+    // Try out a few of the simplified header names.
+    cy.get('.miniparadigm[data-type=plain]').as('paradigm')
+    cy.get('@paradigm')
+      .contains('th', 'you (one) → me')
+      .should('be.visible');
+    cy.get('@paradigm')
+      .contains('th', 'I → you (one)')
+      .should('be.visible');
+    cy.get('@paradigm')
+      .contains('th', 's/he → him/her/them (further)')
+      .should('be.visible');
+  });
 });
