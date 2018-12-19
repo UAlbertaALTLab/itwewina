c77d98eb456338ecc1abf448059c31d451b8a4e9
Eddie Antonio Santos <easantos@ualberta.ca>
2018-12-19 11:05:17 -0700

    Test for loading indicator (#97).

---
+++ neahtta/cypress/integration/features/gh-issue-097-audio-loading-indicator.js
@@ -0,0 +1,37 @@
+/**
+ * There should be indication when the audio is loading.
+ *
+ * See: https://github.com/UAlbertaALTLab/itwewina/issues/97
+ */
+describe('Loading audio', function () {
+  const recordingSearchPattern =
+    /^https?:[/][/]localhost:8000[/]recording[/]_search[/][^/]+$/;
+
+  beforeEach(function () {
+    cy.server();
+  });
+
+  it('should display a loading widget on page load', function () {
+    // Provide our own data for the endpoint.
+    cy.route({
+      url: recordingSearchPattern,
+      response: 'fixture:recording/_search/kostam.json',
+      delay: 1000,
+    })
+      .as('searchRecordings');
+
+    // Get to the details page, where the recordings are actually loaded.
+    cy.instantNeahttaSearch('crk', 'eng', 'kostam');
+    cy.contains('.lexeme:first a', 'kostam').click();
+    cy.url()
+      .should('match', /[/]detail[/].+[/]kostam\b/);
+
+
+    // At this point, there should be a loading indicator.
+    cy.get('.recordings')
+      .contains('Loading');
+
+    // Waiting is over: we have results!
+    cy.wait('@searchRecordings');
+  });
+});
