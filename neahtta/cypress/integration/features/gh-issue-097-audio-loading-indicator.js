b1ed09019a24f50575d96aca675922eacea7278d
Eddie Antonio Santos <easantos@ualberta.ca>
2018-12-19 14:33:07 -0700

    Test loading indicator on recording listing failure.

--- neahtta/cypress/integration/features/gh-issue-097-audio-loading-indicator.js
+++ neahtta/cypress/integration/features/gh-issue-097-audio-loading-indicator.js
@@ -14,6 +14,7 @@ describe('Loading audio', function () {
   it('should display a loading widget on page load', function () {
     // Provide our own data for the endpoint.
     cy.route({
+      method: 'GET',
       url: recordingSearchPattern,
       response: 'fixture:recording/_search/kostam.json',
       delay: 1000,
@@ -39,5 +40,41 @@ describe('Loading audio', function () {
     // The loading indicator should disappear now!
     cy.get('@recordings')
       .should('not.have.descendants', '.loading-indicator');
+  });
+
+  it('should remove the loading widget and notify when no recordings were found', function () {
+    // Provide an endpoint that fails, with a 404.
+    cy.route({
+      method: 'GET',
+      url: recordingSearchPattern,
+      status: 404,
+      response: [],
+      delay: 1000,
+    })
+      .as('searchRecordings');
+
+    // Get to the details page, where the recordings are actually loaded.
+    cy.instantNeahttaSearch('crk', 'eng', 'kostam');
+    cy.contains('.lexeme:first a', 'kostam').click();
+    cy.url()
+      .should('match', /[/]detail[/].+[/]kostam\b/);
+
+    cy.get('.lexeme:first .recordings').as('recordings');
+
+    // It should TRY to load audio.
+    cy.get('@recordings')
+      .get('.loading-indicator')
+      .should('be.visible');
+
+    // Wait for the results to come back.
+    cy.wait('@searchRecordings');
+
+    // The loading indicator should disappear now!
+    cy.get('@recordings')
+      .should('not.have.descendants', '.loading-indicator');
+
+    // There should be a polite message say no recordings have been found.
+    cy.get('@recordings')
+      .contains('No recordings found');
   });
 });
